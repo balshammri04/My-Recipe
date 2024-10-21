b86fae566faa094f7d20512d198ab44709fe4a1b
balshammri04 <bayanalshammri7@gmail.com>
2024-10-21 15:21:02 +0300

    Initial Commit

---
+++ My Recipe/My_RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  My_RecipeApp.swift
+//  My Recipe
+//
+//  Created by bayan alshammri on 18/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct My_RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
